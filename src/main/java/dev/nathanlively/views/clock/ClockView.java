b4daab4e48896810a10640a740bd7335e450ef8e
Nathan Lively <60303904+LiveNathan@users.noreply.github.com>
2024-08-15 21:24:05 -0500

    Add sample events and clean up code in ClockView
    Refactored ClockView to include a method for adding sample events dynamically. Removed unused database initializers in Application and added "@fontsource/anton" dependency in package-lock.json.

--- src/main/java/dev/nathanlively/views/clock/ClockView.java
+++ src/main/java/dev/nathanlively/views/clock/ClockView.java
@@ -5,9 +5,9 @@ import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.button.ButtonVariant;
 import com.vaadin.flow.component.dependency.Uses;
 import com.vaadin.flow.component.grid.Grid;
+import com.vaadin.flow.component.html.ListItem;
 import com.vaadin.flow.component.html.UnorderedList;
 import com.vaadin.flow.component.icon.Icon;
-import com.vaadin.flow.component.orderedlayout.FlexComponent;
 import com.vaadin.flow.component.orderedlayout.FlexComponent.Alignment;
 import com.vaadin.flow.component.orderedlayout.FlexComponent.JustifyContentMode;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
@@ -30,35 +30,48 @@ public class ClockView extends Composite<VerticalLayout> {
 
         Button buttonPrimary = new Button();
         Button buttonSecondary = new Button();
-        UnorderedList evnetsList = new UnorderedList();
+        UnorderedList eventsList = new UnorderedList();
 
         getContent().setWidth("100%");
         getContent().getStyle().set("flex-grow", "1");
         mainRow.addClassName(Gap.MEDIUM);
         mainRow.setWidth("100%");
         mainRow.getStyle().set("flex-grow", "1");
-        column1.getStyle().set("flex-grow", "1");
+
         column1.getStyle().set("flex-grow", "1");
         column1.setJustifyContentMode(JustifyContentMode.CENTER);
         column1.setAlignItems(Alignment.CENTER);
+
         buttonPrimary.setText("Clock In");
-        column1.setAlignSelf(FlexComponent.Alignment.CENTER, buttonPrimary);
-        buttonPrimary.setWidth("min-content");
-        buttonPrimary.addThemeVariants(ButtonVariant.LUMO_PRIMARY);
         buttonSecondary.setText("Clock Out");
-        column1.setAlignSelf(FlexComponent.Alignment.CENTER, buttonSecondary);
+        buttonPrimary.setWidth("min-content");
         buttonSecondary.setWidth("min-content");
+        buttonPrimary.addThemeVariants(ButtonVariant.LUMO_PRIMARY);
+
         column2.getStyle().set("flex-grow", "1");
-        column2.getStyle().set("flex-grow", "1");
-        evnetsList.setWidth("100%");
-        evnetsList.setHeight("100%");
-//        setUnoGridSampleData(basicGrid);
+        eventsList.setWidth("100%");
+        eventsList.setHeight("100%");
+        addSampleEvents(eventsList);
         getContent().add(mainRow);
         mainRow.add(column1);
         column1.add(buttonPrimary);
         column1.add(buttonSecondary);
         mainRow.add(column2);
-        column2.add(evnetsList);
+        column2.add(eventsList);
+    }
+
+    private void addSampleEvents(UnorderedList eventsList) {
+        String[] sampleEvents = {
+                "8/15/24 9:12 IN",
+                "8/15/24 17:12 OUT",
+                "8/16/24 9:10 IN",
+                "8/16/24 17:09 OUT"
+        };
+
+        for (String event : sampleEvents) {
+            ListItem item = new ListItem(event);
+            eventsList.add(item);
+        }
     }
 
     private void setGridSampleData(Grid grid) {
