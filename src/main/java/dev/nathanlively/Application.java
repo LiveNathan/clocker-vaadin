d8476187a9adb4e3c427bb6af045aff9708acd5e
Nathan Lively <60303904+LiveNathan@users.noreply.github.com>
2024-08-15 20:26:52 -0500

    Delete deprecated entity and sample data files
    Removed `AbstractEntity.java`, `data.sql`, and the `dev.nathanlively.data` package-info file as they were no longer used. This cleanup helps to streamline the codebase and remove clutter.

--- src/main/java/dev/nathanlively/Application.java
+++ src/main/java/dev/nathanlively/Application.java
@@ -4,7 +4,7 @@ import com.vaadin.flow.component.dependency.NpmPackage;
 import com.vaadin.flow.component.page.AppShellConfigurator;
 import com.vaadin.flow.theme.Theme;
 import com.vaadin.flow.theme.lumo.Lumo;
-import dev.nathanlively.data.SamplePersonRepository;
+
 import javax.sql.DataSource;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/main/java/dev/nathanlively/views/clock/ClockView.java
+++ src/main/java/dev/nathanlively/views/clock/ClockView.java
@@ -14,12 +14,8 @@ import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
 import com.vaadin.flow.router.RouteAlias;
-import com.vaadin.flow.spring.data.VaadinSpringDataHelpers;
 import com.vaadin.flow.theme.lumo.LumoUtility.Gap;
-import dev.nathanlively.data.SamplePerson;
-import dev.nathanlively.services.SamplePersonService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.PageRequest;
 
 @PageTitle("clock")
 @Route(value = "")
@@ -33,7 +29,7 @@ public class ClockView extends Composite<VerticalLayout> {
         Button buttonPrimary = new Button();
         Button buttonSecondary = new Button();
         VerticalLayout layoutColumn3 = new VerticalLayout();
-        Grid basicGrid = new Grid(SamplePerson.class);
+        Grid basicGrid = new Grid();  // SamplePerson.class
         getContent().setWidth("100%");
         getContent().getStyle().set("flex-grow", "1");
         layoutRow.addClassName(Gap.MEDIUM);
@@ -64,11 +60,11 @@ public class ClockView extends Composite<VerticalLayout> {
     }
 
     private void setGridSampleData(Grid grid) {
-        grid.setItems(query -> samplePersonService.list(
-                PageRequest.of(query.getPage(), query.getPageSize(), VaadinSpringDataHelpers.toSpringDataSort(query)))
-                .stream());
+//        grid.setItems(query -> samplePersonService.list(
+//                PageRequest.of(query.getPage(), query.getPageSize(), VaadinSpringDataHelpers.toSpringDataSort(query)))
+//                .stream());
     }
 
-    @Autowired()
-    private SamplePersonService samplePersonService;
+//    @Autowired()
+//    private SamplePersonService samplePersonService;
 }
